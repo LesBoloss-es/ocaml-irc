3702cd161fbcf9ff528a6524aa1ac16a73704254
Niols <niols@niols.fr>
2018-07-13 12:22:11 +0200

    the internal ppx now almost generates the handler class

--- src/internal_ppx/irc_internal_ppx.ml
+++ src/internal_ppx/irc_internal_ppx.ml
@@ -1,11 +1,84 @@
 
-let type_decl_str ~options ~path type_decls =
-  ignore options;
-  ignore path;
-  ignore type_decls;
-  Format.printf "#options: %d@." (List.length options);
-  Format.printf "#path: %d %s@." (List.length path) (List.hd path);
-  Format.printf "#type_decls: %d@." (List.length type_decls);
-  [%str class handler = object end]
+open Parsetree
+open Asttypes
 
-let () = Ppx_deriving.(register (create "irc_internal_ppx" ~type_decl_str ()))
+let name_of_name_and_suffix name suffix =
+  if name = "t" then
+    suffix
+  else
+    name ^ "_" ^ suffix
+
+let cstr_decl_to_handler_class_method ~options ~path cstr_decl =
+  ignore options; ignore path;
+  {
+    pcf_loc = Location.none ;
+    pcf_attributes = [] ;
+    pcf_desc =
+      Pcf_method (
+          Location.mknoloc ("on_" ^ cstr_decl.pcd_name.txt), Public,
+          Cfk_concrete (Fresh , [%expr ()])
+        )
+  }
+
+let cstr_decls_to_handler_class_main_method ~options ~path name _cstr_decls =
+  ignore options;
+  {
+    pcf_loc = Location.none ;
+    pcf_attributes = [] ;
+    pcf_desc =
+      Pcf_method (
+          Location.mknoloc ("on_" ^ (if name = "t" then List.hd path else name)), Public,
+          Cfk_concrete (Fresh, [%expr ()])
+        )
+  }
+
+let cstr_decls_to_handler_class ~options ~path name cstr_decls =
+  Pstr_class
+    [{
+        pci_virt = Concrete ;
+        pci_params = [] ;
+        pci_name = Location.mknoloc (name_of_name_and_suffix name "handler") ;
+        pci_expr =
+          {
+            pcl_desc =
+              Pcl_structure
+                {
+                  pcstr_self = [%pat? _self];
+                  pcstr_fields =
+                    (cstr_decls_to_handler_class_main_method ~options ~path name cstr_decls)
+                    :: List.map
+                         (cstr_decl_to_handler_class_method ~options ~path)
+                         cstr_decls
+                };
+            pcl_loc = Location.none ;
+            pcl_attributes = []
+          } ;
+        pci_loc = Location.none ;
+        pci_attributes = []
+    }]
+
+
+let type_decl_to_everything ~options ~path = function
+  (* We expect exactly one type declaration. *)
+  | [type_decl] ->
+     (
+       match type_decl.ptype_kind with
+       (* This type declaration must be a variant. *)
+       | Ptype_variant cstr_decls ->
+          [{
+              pstr_desc =
+                cstr_decls_to_handler_class
+                  ~options
+                  ~path
+                  type_decl.ptype_name.txt
+                  cstr_decls ;
+              pstr_loc =
+                Location.none
+          }]
+       | _ ->
+          assert false
+     )
+  | _ ->
+     assert false
+
+let () = Ppx_deriving.(register (create "irc_internal_ppx" ~type_decl_str:type_decl_to_everything ()))
